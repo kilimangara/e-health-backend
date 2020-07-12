77a746fabfa8c1472e6c2dcf395667ef307bc479
Davide Zgyatti <duzgyatti@avito.ru>
2020-07-12 15:10:06 +0300

    Получилось какое-то говно

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# Import all the models, so that Base has them before being
+# imported by Alembic
+from app.db.base_class import Base  # noqa
+from app.models.item import Item  # noqa
+from app.models.user import User  # noqa
